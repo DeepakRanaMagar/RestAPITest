4f728d675a2f9bfc62b1913cce8c2c74b02207c6
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-01-01 14:51:08 +0545

    .

--- posts/views.py
+++ posts/views.py
@@ -5,13 +5,16 @@ from django.shortcuts import render
 
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 class PostList(generics.ListCreateAPIView):
     queryset= Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (
+        permissions.IsAdminUser,
+    )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
