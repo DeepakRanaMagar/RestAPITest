7000114c99e55a1038a74c4541c694d1bf9fdffe
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-01-01 11:34:09 +0545

    insom

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# ListCreateAPIView(built-in) -> read-write endpoint(POST request and READ request) 
+# RetrieveUpdateDestroyAPIView(built-in generics)-> CRUD
+
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import generics
+
+class PostList(generics.ListCreateAPIView):
+    queryset= Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
